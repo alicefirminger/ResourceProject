c5f4849a151aca5ca13ac191498f51302f9b6057
Sam Baldwin <sambaldwin92@gmail.com>
2023-04-26 11:29:05 +0100

    Created new navbar component

--- myapp/src/Components/App/App.js
+++ myapp/src/Components/App/App.js
@@ -1,13 +1,37 @@
 import React from "react";
 import "./App.css";
-import Container from "@mui/material/Container";
 import FilterDropDown from "../FilterDropdown/FilterDropDown";
+//Navbar Imports
+import AppBar from "@mui/material/AppBar";
+import Toolbar from '@mui/material/Toolbar';
+import { Box } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <FilterDropDown />
-    </Container>
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h1"
+            component="div"
+            sx={{ flexGrow: 1, fontSize: "1rem" }}
+          >
+            Bootcamper's Treasure Chest
+          </Typography>
+          <FacebookRoundedIcon sx={{ mr: 1 }} />
+          <LinkedInIcon sx={{ mr: 1 }} />
+          <GitHubIcon sx={{ mr: 1 }} />
+        </Toolbar>
+      </AppBar>
+
+      <Box>
+        <FilterDropDown />
+      </Box>
+    </Box>
   );
 }
 
