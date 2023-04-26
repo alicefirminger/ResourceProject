0d065f768093827ca63d8f91af3063ad572c00df
Sam Baldwin <sambaldwin92@gmail.com>
2023-04-26 13:15:56 +0100

    Refactored the navbar to a component file

--- myapp/src/Components/App/App.js
+++ myapp/src/Components/App/App.js
@@ -1,38 +1,18 @@
 import React from "react";
 import "./App.css";
 import FilterDropDown from "../FilterDropdown/FilterDropDown";
+import Navbar from "../Navbar/Navbar";
+// import Navbar from "../Navbar/Navbar";
 //Navbar Imports
-import AppBar from "@mui/material/AppBar";
-import Toolbar from '@mui/material/Toolbar';
-import { Box } from "@mui/material";
-import Typography from "@mui/material/Typography";
-import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import GitHubIcon from "@mui/icons-material/GitHub";
+import Box from "@mui/material/Box";
 
 function App() {
   return (
     <Box>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography
-            variant="h1"
-            component="div"
-            sx={{ flexGrow: 1, fontSize: "1rem" }}
-          >
-            Bootcamper's Treasure Chest
-          </Typography>
-          <FacebookRoundedIcon sx={{ mr: 1 }} />
-          <LinkedInIcon sx={{ mr: 1 }} />
-          {/* <GitHubIcon sx={{ mr: 1 }} /> */}
-        </Toolbar>
-      </AppBar>
-
-      <Box>
-        <FilterDropDown />
-      </Box>
+      <Navbar />
+      <FilterDropDown />
     </Box>
   );
 }
 
-  export default App;
+export default App;
--- myapp/src/Components/FilterDropdown/FilterDropDown.js
+++ myapp/src/Components/FilterDropdown/FilterDropDown.js
@@ -2,30 +2,42 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
-function FilterDropDown(props)
-{
-
+function FilterDropDown(props) {
   function makeAlert(value) {
     alert(`You clicked ${value}!`);
   }
 
   return (
-    <FormControl sx={{ mt: 2, width: 0.4 }}>
-      <InputLabel id="filter-menu">Select Category</InputLabel>
-      <Select
-        // labelId="filter-menu"
-        // id="filter-menu"
-        // value={""}
-        label="Select Category"
-        onChange={(event) => makeAlert(event.target.value)}
+    <Box
+      bgcolor={"#FCF7F1"}
+      sx={{ height: "200px" }}
+    >
+      <Typography
+        variant="h1"
+        component="div"
+        sx={{ flexGrow: 0, fontSize: "1rem", color: "#004777" }}
       >
-        <MenuItem value={"css"}>CSS</MenuItem>
-        <MenuItem value={"react"}>React</MenuItem>
-        <MenuItem value={"javascript"}>Javascript</MenuItem>
-        <MenuItem value={"mindset"}>Mindset</MenuItem>
-      </Select>
-    </FormControl>
+        Resources
+      </Typography>
+      <FormControl sx={{ mt: 2, width: 0.4 }}>
+        <InputLabel id="filter-menu">Select Category</InputLabel>
+        <Select
+          // labelId="filter-menu"
+          // id="filter-menu"
+          // value={""}
+          label="Select Category"
+          onChange={(event) => makeAlert(event.target.value)}
+        >
+          <MenuItem value={"css"}>CSS</MenuItem>
+          <MenuItem value={"react"}>React</MenuItem>
+          <MenuItem value={"javascript"}>Javascript</MenuItem>
+          <MenuItem value={"mindset"}>Mindset</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
   );
 }
 
