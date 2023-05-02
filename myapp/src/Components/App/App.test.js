be893f0de758e573bfda101fb52301a5cd73d72c
alicefirminger <alicefirminger@gmail.com>
2023-05-02 16:50:21 +0100

    Trying to get a react test working, really not understanding it so far

--- myapp/src/Components/App/App.test.js
+++ myapp/src/Components/App/App.test.js
@@ -1,8 +1,14 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ResourceListDisplay from '../ResourceList/ResourceList.js'
+import resource from '../Resource/Resource.js'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('checks that the links are displayed on the page', () => {
+  render(<ResourceListDisplay resource={resource} />);
+  resource.forEach((item) => {
+    const linkElement = screen.getByAltText(item.imgAlt);
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute('href', item.link);
+  });
 });
